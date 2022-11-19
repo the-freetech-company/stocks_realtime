8f0ee5bf4c60d16f20f7a4d5908b049ea24e74e5
adamsiwiec1 <adam@siwiectech.com>
2022-11-19 13:50:07 -0500

    volumes_update

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
-bash docker_install_linux.sh -y && \
-bash volumes.sh && \
+bash scripts/docker_install.sh -y && \
+bash scripts/volumes.sh && \
+bash scripts/portainer.sh && \  # portainer used for ease of monitoring
 docker compose -f mark-compose.yml up -d
