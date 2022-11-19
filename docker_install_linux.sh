e0a62151bfeabb88ce4dd74f83ae3e0beef511b4
adamsiwiec1 <adam@siwiectech.com>
2022-11-19 13:24:41 -0500

    volumes_update

--- docker_install_linux.sh
+++ docker_install_linux.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y && \
 sudo apt-get install ca-certificates curl gnupg lsb-release -y && \
-sudo mkdir -p /etc/apt/keyrings && \
+sudo mkdir -p /etc/apt/keyrings -y && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
